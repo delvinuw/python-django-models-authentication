ec56252e6ec805023c2935aca525ebac93b12978
delvinuw <delvin@uw.edu>
2020-03-22 16:16:33 -0700

    Completed module

--- mainapp/migrations/0003_auto_20200322_2225.py
+++ mainapp/migrations/0003_auto_20200322_2225.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
             name='BlogPost',
         ),
         migrations.DeleteModel(
-            name='Tag',
+            name='Tag', 
         ),
     ]
