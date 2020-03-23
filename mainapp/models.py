57152ee94d7633176268670cb425f5ac587f55eb
delvinuw <delvin@uw.edu>
2020-03-22 17:46:50 -0700

    module3 2nd

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 class BlogPost(models.Model):
     title = models.CharField(max_length = 200, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="post", on_delete=models.CASCADE)
+    body = models.TextField()
+    postdate = models.DeateTimeField(auto_now_add=True, blank=True)
+    
+    def get_absolute_url(self):
+        return reverse('post', args=[str(self.id)])
+    
+    
+    def __str__(self):
+        self.title = title
+        return self.title
+    
+        
     
     
     
