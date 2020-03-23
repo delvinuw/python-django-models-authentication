d1819f59568a6cad166d3d79b211a2a51770ffe3
delvinuw <delvin@uw.edu>
2020-03-22 17:28:36 -0700

    module 3 initial

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,2 +1,11 @@
 from django.db import models
+from django.conf import settings
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length = 200, unique=True)
+    
+    
+    
+    
+    
 
